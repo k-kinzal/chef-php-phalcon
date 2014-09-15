79824b5829089c8d2003557de2b239b960d4fbfa
Jakub Riedl <jakub.riedl@le.cz>
2014-09-15 13:38:33 +0200

    edit metadata, change condition in attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,14 @@
 default['php-phalcon']['git_url'] = 'git://github.com/phalcon/cphalcon.git'
 default['php-phalcon']['git_ref'] = 'master'
 
-if platform_family?("rhel", "fedora")
+case platform_family
+when "rhel", "fedora"
 
     default['php-phalcon']['packages'] = %w[git php-devel pcre-devel gcc make]
     default['php-phalcon']['conf_dir'] = '/etc/php.d'
     default['php-phalcon']['conf_file'] = 'phalcon.ini'
 
-elsif platform_family?("debian")
+when "debian"
 
     default['php-phalcon']['packages'] = %w[git php5-dev libpcre3-dev gcc make php5-mysql]
     default['php-phalcon']['conf_dir'] = '/etc/php.d'
